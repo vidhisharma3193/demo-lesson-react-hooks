8b998755bed7f039fb1ff1f9e4722bef84c10bc6
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-05-20 17:35:41 -0500

    Initial code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import Counter from './Counter';
+
+function App() {
+  return (
+       <div>
+        <Counter />
+      </div>
+  );
+}
+
+export default App;
